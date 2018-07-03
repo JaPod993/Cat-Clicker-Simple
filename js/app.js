b3d0d859087c8f7cd7d886ede51f642481a65597
Jan Podgorski <j.pod.12@gmail.com>
2018-07-03 10:27:13 +0200

    aga

--- js/app.js
+++ js/app.js
@@ -1,21 +1,51 @@
 document.addEventListener("DOMContentLoaded",function(){
 
     let cats = {
-        cat : document.querySelectorAll('.cats'),
         clicks : document.querySelectorAll('.clicks'),
         name : document.querySelectorAll('.cat-name'),
-        click : 0
+
+
+        names : ["Bonifacy", "Filemon", "Pankracy", "Mruczek", "Tygrysek"],
+        files : ["cat1", "cat2", "cat3", "cat4", "cat5"],
+        click : 0,
+        list : document.querySelector('.cats-list'),
+        clickedCat : document.querySelector('.cat-clicker'),
+        fragment : document.createDocumentFragment(),
+        cat : document.querySelectorAll('.list')
     };
 
-    cats.name[0].textContent = "Bonifacy";
-    cats.name[1].textContent = "Filemon";
+    //creating list of cats
+    function list() {
+        cats.names.forEach(function (value) {
+            let kitty = document.createElement('p');
+            kitty.classList = 'list';
+            let singleCatList = value;
+            kitty.innerHTML = singleCatList;
+            cats.fragment.appendChild(kitty);
+        });
 
-    for(let i = 0; i<=1; i++){
-        cats.cat[i].addEventListener('click', (function (clickCopy) {
-            return function() {
-                clickCopy += 1;
-                cats.clicks[i].textContent = clickCopy + " clicks";
-            };
-        })(cats.click));
+        cats.list.appendChild(cats.fragment);
+        cats.fragment.innerHTML ='';
     }
+    list();
+
+    //creating cats
+    function kittens() {
+        cats.files.forEach(function (value, index) {
+            let kitty = document.createElement('div');
+            kitty.classList = 'kitty-wrapper'+ index;
+            let singleCat = '<p class="cat-name">' + cats.names[index] +'</p><img src="images/' + value + '.jpg" alt="cat" class="cats"><p class="clicks">0 clicks</p>';
+            kitty.innerHTML = singleCat;
+            cats.fragment.appendChild(kitty);
+        });
+        cats.clickedCat.appendChild(cats.fragment);
+    }
+    kittens();
+
+    // event for clicking cats
+    cats.cat.forEach(function (value) {
+       value.addEventListener('click', function () {
+           kotek();
+       });
+    });
 });
